42f6c26d0a989cd661ab311f3a2d9f881d3fc231
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-03-28 21:00:17 +0100

    fix: test URL path

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from blog import views as main_views
 
 urlpatterns = [
-    path('', main_views.my_blog, name='main'),
+    path('blog/', main_views.my_blog, name='main'),
     path('admin/', admin.site.urls),
 ]
