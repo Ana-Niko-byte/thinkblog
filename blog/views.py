c61f74faff7628b4a9f299db6124d1c392f76976
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-01 14:30:27 +0200

    add: template for posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-# Create your views here.
-# def main(response):
-#     if response.method == 'GET':
-#         return HttpResponse('Hey there :)')
-#     else:
-#         return response
-def my_blog(request):
-    return HttpResponse("Hello, Blog! This is working right?")
+class PostList(generic.ListView):
+    # model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
