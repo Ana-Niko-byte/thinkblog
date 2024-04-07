7b632824e4683fae3c5070c079c399d81f7d7da8
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-07 18:54:01 +0100

    add: add Creator model + test view

--- about/models.py
+++ about/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+# step 1 after creating the model
 # Create your models here.
+class creator(models.Model):
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    updated_on=models.DateTimeField(auto_now=True)
+
+    # for better readability in admin.
+    def __str__(self):
+        return self.title
