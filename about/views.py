b406671aff6e8723222547ca2ef497ca43d4f675
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-04-11 11:58:13 +0100

    add: add collaboration request form + backend

--- about/views.py
+++ about/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from .models import Creator
-from django.http import HttpResponse
+from .forms import CollaborateForm
+from django.contrib import messages
 # Create your views here.
 
 def about_page(request):
+    if request.method == 'POST':
+        print('received collaboration request')
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            c_form = collaborate_form.save(commit=False)
+            c_form.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Collaboration request received! I endeavour to respond within 2 working days.'
+    )
     author = Creator.objects.all().order_by("-updated_on").first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {'about' : author},
+        {'about' : author, 
+         "collaborate_form": collaborate_form,}
     )
--- blog/models.py
+++ blog/models.py
@@ -28,6 +28,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, 
+        # name used in views on instance retrieval
         related_name = "comments"
         )
     author = models.ForeignKey(
